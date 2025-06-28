2ddd85292225ca1f45593c680f2c413ce5fcaea4
MICHAŁ <mg814@wp.pl>
2025-06-28 12:53:08 +0200

    fix: fix ruff error - change init file in tests

--- src/payments/tests/__init__.py
+++ src/payments/tests/__init__.py
@@ -1,3 +1,9 @@
 from .test_checkout_session import TestCreateCheckoutSessionView
 from .test_notify_stripe_view import TestNotifyStripeView
 from .test_send_email import TestPaymentsFunctions
+
+__all__ = [
+    'TestCreateCheckoutSessionView',
+    'TestNotifyStripeView',
+    'TestPaymentsFunctions'
+]
