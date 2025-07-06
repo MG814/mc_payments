60f4b4a6aee24066014973842993bdc36a7b8e71
MICHAŁ <mg814@wp.pl>
2025-07-06 17:47:37 +0200

    feat: add new fields in payments models and save payments in database

--- src/payments/models.py
+++ src/payments/models.py
@@ -6,3 +6,8 @@ class Payments(models.Model):
     doctor_id = models.IntegerField()
     title = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stripe_session_id = models.CharField(max_length=255, null=True, blank=True)
+    visit_id = models.IntegerField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
